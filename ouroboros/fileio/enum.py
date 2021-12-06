8cd1e22281fe1615682a1caa5746a397f7c3fa27
Ouroboros <Hiromi.Kaede@gmail.com>
2021-12-06 13:54:03 +0800

    getDirectoryFiles subdir

--- ouroboros/fileio/enum.py
+++ ouroboros/fileio/enum.py
@@ -1,18 +1,23 @@
 import os, glob, fnmatch
 
-def getDirectoryFiles(path, filter = '*.*'):
+def getDirectoryFiles(path, filter = '*.*', *, subdir = True):
     # allfiles = []
     if filter == '*.*':
         filter = '*'
     elif not filter.startswith('*'):
         filter = '*' + filter
 
-    for root, dirs, files in os.walk(path):
-        for f in files:
-            f = os.path.join(root, f)
-            if fnmatch.filter([f], filter) and not os.path.isdir(f):
-                yield f
+    if subdir:
+        for root, dirs, files in os.walk(path):
+            for f in files:
+                f = os.path.join(root, f)
+                if fnmatch.filter([f], filter) and not os.path.isdir(f):
+                    yield f
 
             # fnmatch.filter([f], filter) and not os.path.isdir(f) and allfiles.append(f)
 
-    # return allfiles
+    else:
+        for f in os.listdir(path):
+            f = os.path.join(path, f)
+            if fnmatch.filter([f], filter) and not os.path.isdir(f):
+                yield f
